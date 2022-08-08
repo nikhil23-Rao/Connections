15665569326359339ad49e1cb54c3ea9564289a4
Nikhil Rao <nikhil23.rao@gmail.com>
2022-08-08 10:56:56 -0700

    new btn design

--- web/components/Landing/Button.tsx
+++ web/components/Landing/Button.tsx
@@ -1,7 +1,14 @@
 export const Button = () => {
   return (
     <>
-      <a className="btn btn-1">Hover me</a>
+      <div className="buttons">
+        <button
+          className="btn-hover color-1"
+          style={{ width: 400, height: 200 }}
+        >
+          BUTTON
+        </button>
+      </div>
     </>
   );
 };
